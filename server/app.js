8539f2ad4955bc98dbe0eae3fbd2ecd65b6cbd9b
Julian Hollmann <julian.hollmann@googlemail.com>
2013-03-19 17:51:31 +0100

    server without logic

---
+++ server/app.js
@@ -0,0 +1,31 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+	, logs = require('./routes/logs')
+	, http = require('http')
+	, path = require('path');
+
+var app = express();
+
+app.configure(function ()
+{
+	app.set('port', process.env.PORT || 3000);
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname + '/../', 'client/')));
+});
+
+app.configure('development', function ()
+{
+	app.use(express.errorHandler());
+});
+
+http.createServer(app).listen(app.get('port'), function ()
+{
+	console.log("Logger server listening on port " + app.get('port'));
+});
