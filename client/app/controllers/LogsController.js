e95a4d414804509f479b39345b53f37ff311ffed
Julian Hollmann <julian.hollmann@googlemail.com>
2013-03-25 22:24:34 +0100

    Awesome frontend

--- client/app/controllers/LogsController.js
+++ client/app/controllers/LogsController.js
@@ -1,7 +1,5 @@
 LiveLogger.Controller.LogsController = function(Log, $scope) {
+	$scope.keys = Log.getKeys();
 	$scope.logs = Log.get();
-
-	$scope.endAmqpConnection = function() {
-		Log.endAmqpConnection();
-	}
+    $scope.keyFilter = '';
 };
--- client/app/services/LogService.js
+++ client/app/services/LogService.js
@@ -1,11 +1,38 @@
 angular.module('LiveLogger', [], function($provide) {
 	$provide.factory('Log', ['$rootScope', function($rootScope) {
+
+        var limit = 20;
+
 		var logs = [];
+        var keys = {};
 
 		var socket = io.connect('http://127.0.0.1:3001');
 		socket.on('log', function (data) {
-			console.log(data);
-			logs.push(data);
+
+            for(var key in data) {
+                if(logs.length + 1 > limit) {
+                    logs.splice(0, 1);
+                }
+
+                if(typeof keys[key] === 'undefined') {
+                    var color = "#"+((1<<24)*Math.random()|0).toString(16);
+                    keys[key] = color;
+                }
+
+                for(var hash in data[key]) {
+                    var newData = {
+                        hash: hash,
+                        key: key,
+                        data: data[key][hash][0],
+                        count: data[key][hash].length,
+                        color: keys[key]
+                    };
+
+                    logs.push(newData);
+                    console.log(newData);
+                }
+            }
+
 			if (!$rootScope.$$phase)
 			{
 				$rootScope.$apply();
@@ -16,6 +43,15 @@ angular.module('LiveLogger', [], function($provide) {
 			get: function() {
 				return logs;
 			},
+            getKeys: function() {
+                return keys;
+            },
+            getLimit: function() {
+                return limit;
+            },
+            setLimit: function(showLimit) {
+                return limit = showLimit;
+            },
 			endAmqpConnection: function() {
 				socket.emit('endAmqpConnection');
 			}
